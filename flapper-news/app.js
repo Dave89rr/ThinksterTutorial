c940cd36c81156c2361384a0005faf057bb20b87
Dave89rr <daleriro@msn.com>
2016-01-30 17:41:17 -0500

    Revert "1.2.2"
    This reverts commit 2af00a1898620bf0062e9a641abfbfdba6e28557.

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-require('.models/Posts');
-require('.models/Comments');
 
 mongoose.connect('mongodb:/localhost/news')
 
--- flapper-news/models/posts.js
+++ flapper-news/models/posts.js
@@ -1,10 +1,5 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-  title: String,
-  link: String,
-  upvotes: {types: Number, default: 0},
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
-});
 
-mongoose.model('Post', PostSchema);
+})
