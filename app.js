139f4917cac86ee0366786d6df7bfaf76248a335
Dave89rr <daleriro@msn.com>
2016-01-30 10:23:35 -0500

    Added Comments
    Added option to view comments in individual posts

--- app.js
+++ app.js
@@ -58,5 +58,16 @@ app.controller('PostsCtrl', [
   '$stateParams',
   'posts',
   function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
 
+    $scope.addComment = function(){
+      if (!$scope.body || $scope.body === '') { return; }
+
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0,
+        })
+      $scope.body = '';
+    };
   }]);
