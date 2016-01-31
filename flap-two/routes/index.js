74b50744109938c505f5d377674335bd3763ef3b
Dave89rr <daleriro@msn.com>
2016-01-31 01:42:37 -0500

    Update 1.4

--- flap-two/routes/index.js
+++ flap-two/routes/index.js
@@ -6,4 +6,62 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) { next(err); }
+
+    res.json(posts);
+  });
+});
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("Can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:posts/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment) {
+    if (err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if (err) { return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
 module.exports = router;
